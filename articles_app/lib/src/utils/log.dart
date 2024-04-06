8148c102afa72d55b4e73eb573099f925961adcc
koshitake2m2 <blueling2m2dvp@gmail.com>
2024-04-06 16:23:59 +0900

    logger wip

--- articles_app/lib/src/utils/log.dart
+++ articles_app/lib/src/utils/log.dart
@@ -3,12 +3,19 @@ import 'package:logging/logging.dart' as logging;
 
 class Log {
   // https://github.com/dart-lang/logging
-  static final _logger = logging.Logger('MyLogger');
+  static final _logger = _initLogger();
+
+  static logging.Logger _initLogger() {
+    final logger = logging.Logger('MyLogger');
+    logging.Logger.root.level = logging.Level.ALL;
+    logging.Logger.root.onRecord.listen((logging.LogRecord rec) {
+      // ignore: avoid_print
+      print('[${rec.level.name}]: ${rec.time}: ${rec.message}');
+    });
+    return logger;
+  }
 
   static void info(String message) {
     _logger.info(message);
-    if (kDebugMode) {
-      print(message);
-    }
   }
 }
